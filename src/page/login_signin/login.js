7bee66b4f5edfb8811c992afa42bb30ee469c6ee
MinhBLues <49396338+MinhBLues@users.noreply.github.com>
2020-08-04 01:05:19 +0700

    edit layout SignIn,  SignUp

--- src/page/login_signin/login.js
+++ src/page/login_signin/login.js
@@ -1,86 +1,117 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import "./login.css";
-import "./nouislider.min.css";
-export default class CreateUser extends Component {
-    constructor(props) {
-        super(props)
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-        this.state = {
-            email: '',
-            password:''
-        }
-    }
-    onChangeEmail(e) {
-        this.setState({ email: e.target.value })
-    }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    onChangePassword(e) {
-        this.setState({ password: e.target.value })
-    }
-    onSubmit(e) {
-        e.preventDefault()
+export default function SignIn() {
+  const classes = useStyles();
 
-        const userObject = {
-
-            email: this.state.email,
-            password:this.state.password
-        };
-
-        axios.post('https://scheduleapi.herokuapp.com/user/login', userObject,{
-            headers:{
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            },responseType: 'json'})
-            .then((res) => {
-                console.log(res.data.token)
-            }).catch((error) => {
-                console.log(error)
-            });
-            alert('your account created');
-        this.setState({ email: '',password:'' })
-    }
-
-    // render(){
-    // return (
-    //     <div className="wrapper">
-    //     <form onSubmit={this.onSubmit}>
-    //         <div className="form-group">
-    //             <label>  Email</label>
-    //             <input type="text" value={this.state.email} onChange={this.onChangeEmail} className="form-control" />
-    //         </div>
-    //         <div className="form-group">
-    //             <label>Password</label>
-    //             <input type="password" value={this.state.password} onChange={this.onChangePassword} className="form-control" />
-    //         </div>
-    //         <div className="form-group">
-    //             <input type="submit" value="Create User" className="btn btn-primary" />
-    //         </div>
-    //     </form>
-    // </div>
-    // )
-    // }
-    render(){
-        return (
-            <div class="container">
-                <div class="login-form">
-                        <h3 class="billing-title text-center">Đăng nhập</h3>
-                        <p class="text-center mt-80 mb-40">Chào mừng trở lại! đăng nhập vào tài khoản của bạn </p>
-                        <div class="alert alert-danger login-fail" role="alert">
-                            Tài khoản hoặc mật khẩu không chính xác
-                        </div>
-                        <input type="text" placeholder="Tên tài khoản*" name="lg_username" class="input"/>
-                        <input placeholder="Mật khẩu*" name="lg_password" class="input mt-20"/>
-                        <button class="view-btn color-2 mt-20 w-100 btn-login" ><span>Đăng nhập</span></button>
-                        <div class="mt-20 d-flex align-items-center justify-content-between text-center">
-                            <a href="/forgetpass">Quên mật khẩu?</a>
-                        </div>
-                </div>
-        </div>
-        )
-        }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
--- src/page/login_signin/signin.js
+++ src/page/login_signin/signin.js
@@ -1,85 +1,148 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default class CreateUser extends Component {
-    constructor(props) {
-        super(props)
-        this.onChangeName=this.onChangeName.bind(this);
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onChangeRepeadPW=this.onChangeRepeadPW.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-        this.state = {
-            name:'',
-            email: '',
-            password:'',
-            repeat_password:''
-        }
-    }
-    onChangeName(e){
-        this.setState({name: e.target.value})
-    }
-    onChangeEmail(e) {
-        this.setState({ email: e.target.value })
-    }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-    onChangePassword(e) {
-        this.setState({ password: e.target.value })
-    }
-    onChangeRepeadPW(e) {
-        this.setState({ repeat_password: e.target.value })
- 
-    }
+export default function SignUp() {
+  const classes = useStyles();
 
-    onSubmit(e) {
-        e.preventDefault()
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="repeat_password"
+                label="Repeat Password"
+                type="password"
+                id="repeat_password"
+                value = "this.state.repeat_password"
+                autoComplete="current-reapeat-password"
+              />
+            </Grid>
 
-        const userObject = {
-            name:this.state.name,
-            email: this.state.email,
-            password:this.state.password,
-            repeat_password:this.state.password
-        };
-
-        axios.post('https://scheduleapi.herokuapp.com/user/register', userObject,{
-            headers:{
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-            },responseType: 'json'})
-            .then((res) => {
-                console.log(res.data)
-            }).catch((error) => {
-                console.log(error)
-            });
-            alert('your account created');
-        this.setState({ name: '',email: '',password:'',repeat_password:'' })
-    }
-    render(){
-    return (
-        <div className="wrapper">
-        <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-                <label>  Name</label>
-                <input type="text" value={this.state.name} onChange={this.onChangeName} className="form-control" />
-            </div>
-            <div className="form-group">
-                <label>  Email</label>
-                <input type="text" value={this.state.email} onChange={this.onChangeEmail} className="form-control" />
-            </div>
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" value={this.state.password} onChange={this.onChangePassword} className="form-control" />
-            </div>
-            <div className="form-group">
-                <label>repeat_password number </label>
-                <input type="text" value={this.state.repeat_password} onChange={this.onChangeRepeadPW} className="form-control" />
-            </div>
-            <div className="form-group">
-                <input type="submit" value="Create User" className="btn btn-primary" />
-            </div>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
         </form>
-    </div>
-    )
-    }
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
